5bc457029a5024d3ae0c1129528b331ded6cec57
Marcos G. S. C. Sete <marcossetecruzsoares@gmail.com>
2024-06-05 11:49:55 -0300

    improving formatting

--- projetopii331/src/main/java/entities/Municipio.java
+++ projetopii331/src/main/java/entities/Municipio.java
@@ -260,11 +260,11 @@ public class Municipio {
 
     @Override
     public String toString() {
-        return codigoIBGE + "," + municipio + "," + microrregiao + "," + estado + "," + regiaoGeografica + "," +
-                area + "," + populacao + "," + domicilios + "," + pibTotal + "," + idh + "," + rendaMedia + "," +
-                rendaNominal + "," + peaDia + "," + idhEducacao + "," + idhLongevidade + "," +
-                densidadeDemografica + "," + pibPerCapita + "," + classificacaoIDH + "," +
-                classificacaoIDHEducacao + "," + classificacaoIDHLongevidade + "," + ultimaAtualizacao;
+        return "codigoIBGE: "+codigoIBGE + " , " + "municipio: "+municipio + " , " +"microrregiao: "+microrregiao + " , " +"estado: "+ estado + " , " + "regiaoGeografica: "+regiaoGeografica + " , " +
+                "area: "+area + " , " +"populacao: "+ populacao + " , " +"domicilios: "+ domicilios + " , " +"pibTotal: "+ pibTotal + " , " + "idh: "+idh + " , " + "rendaMedia"+rendaMedia + " , " +
+                "rendaNominal: "+rendaNominal + " , " + "peaDia: "+peaDia + " , " +"idhEducacao: "+ idhEducacao + " , " + "idhLongevidade: "+idhLongevidade + " , " +
+                "densidadeDemografica: "+densidadeDemografica + " , " + "pibPerCapita: "+pibPerCapita + " , " + "classificacaoIDH: "+classificacaoIDH + " , " +
+                "classificacaoIDHEducacao: "+classificacaoIDHEducacao + " , " + "classificacaoIDHLongevidade: "+ classificacaoIDHLongevidade + " , " +"ultimaAtualizacao: "+ ultimaAtualizacao;
     }
     
     
